24ba58fb19f03f595193d5b6b572a7e85574244e
Emanuel Taborda <emanuel.cristotaborda@gmail.com>
2026-03-26 22:49:54 -0300

    Definição de consulta persolizada no repository, para filtrar por nome de produto

--- src/main/java/com/DevSuperior/DSCommerce/repositories/ProductRepository.java
+++ src/main/java/com/DevSuperior/DSCommerce/repositories/ProductRepository.java
@@ -1,7 +1,15 @@
 package com.DevSuperior.DSCommerce.repositories;
 
 import com.DevSuperior.DSCommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query(value = "SELECT * FROM tb_product p " +
+            "WHERE UPPER(p.name) LIKE UPPER(CONCAT('%', :name, '%'))", nativeQuery = true)
+    Page<Product> findByName(String name, Pageable pageable);
+
 }
