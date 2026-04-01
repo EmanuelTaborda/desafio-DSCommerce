57cb05d2acf004e9e0ddc0123d601cd2f445b090
Emanuel Taborda <emanuel.cristotaborda@gmail.com>
2026-03-31 21:55:31 -0300

    Load user by email

--- src/main/java/com/DevSuperior/DSCommerce/services/UserService.java
+++ src/main/java/com/DevSuperior/DSCommerce/services/UserService.java
@@ -26,6 +26,18 @@ public class UserService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return null;
+		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+
+		if (result.isEmpty()) {
+			throw new UsernameNotFoundException("Email not found");
+		}
+
+		User user = new User();
+		user.setEmail(result.get(0).getUsername());
+		user.setPassword(result.get(0).getPassword());
+		for (UserDetailsProjection projection : result) {
+			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+		}
+		return user;
 	}
 }
